6b50cc125cedc7e509b6db6b7d2646a24c21890a
yash-0025 <yashpatel200403@gmail.com>
2025-06-27 19:17:34 +0530

    # CONTRACTS :: DEMASKED TOKEN CONTRACT BUY FUNCTIONS UPDATED✅

--- contracts/src/DeMaskedToken.sol
+++ contracts/src/DeMaskedToken.sol
@@ -6,8 +6,39 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DeMaskedToken is ERC20, Ownable {
 
+    uint256 public ethToDmtRate;
+
+    event TokensPurchased(address indexed buyer, uint256 ethAmount, uint256 dmtAmount);
+    event EthToDmtRateUpdated(uint256 newRate);
+
+
     constructor() ERC20("DeMasked Token", "DMT") Ownable(msg.sender) {
         _mint(msg.sender, 100_000_000 * 10 ** decimals()); // 100 million DMT mints
+        ethToDmtRate = 100_000 * 10**decimals();
+        emit EthToDmtRateUpdated(ethToDmtRate);
+    }
+
+    function buyTokens() public payable {
+        require(msg.value > 0, "Must send Ether to buy tokens");
+        require(ethToDmtRate > 0, "Token purchase is currently diabled or rate is zero");
+
+        uint256 dmtToMint = (msg.value * ethToDmtRate) / 10**18;
+        require(dmtToMint > 0, "Calculated DMT amount is zero");
+
+        _mint(msg.sender, dmtToMint);
+        emit TokensPurchased(msg.sender, msg.value, dmtToMint);
+    }
+
+    function setEthToDmtRate(uint256 _newRate) public onlyOwner {
+        ethToDmtRate = _newRate;
+        emit EthToDmtRateUpdated(_newRate);
+    }
+
+    function withdrawEth() public onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No Ether to withdraw");
+        (bool success,) = owner().call{value: balance}("");
+        require(success, "Ether withdrawal failed");
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
