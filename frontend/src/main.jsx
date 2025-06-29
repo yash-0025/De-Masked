db331b612e3114a65c6e1b1938b7d993989b5050
yash-0025 <yashpatel200403@gmail.com>
2025-06-30 02:18:49 +0530

    # FRONTEND :: WAGMI CONFIG INITIALIZED IN MAIN.JSX ✅

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,44 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createConfig, WagmiProvider} from 'wagmi';
+import {sepolia} from 'wagmi/chains';
+import {http} from 'viem';
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import {WalletConnectConnector} from 'wagmi/connectors/walletConnect';
+import {CoinbaseWalletConnector} from 'wagmi/connectors/coinbaseWallet';
+
+
+const projectId = import.meta.env.VITE_PROJECT_ID
+
+const chains = [sepolia]
+
+const wagmiConfig = createConfig({
+  chains,
+  transports: {
+    [sepolia.id]: http(),
+  },
+  connectors: [
+    new InjectedConnector({chains}),
+    new WalletConnectConnector({
+      chains, options: {
+        projectId,
+        showQrModal: true,
+      },
+    }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'DeMasked',
+      },
+    }),
+  ],
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <WagmiProvider config={wagmiConfig}>
     <App />
+    </WagmiProvider>
   </StrictMode>,
 )
