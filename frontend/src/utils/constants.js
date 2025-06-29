927854e622700f6fdf883c2bc633ed1478a4a7f2
yash-0025 <yashpatel200403@gmail.com>
2025-06-30 01:42:01 +0530

    # FRONTEND :: CONSTANTS FILE ADDED ✅

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,16 @@
+import DeMaskedTokenABI from '../abi/DeMaskedToken.json';
+import DeMaskedCoreABI from '../abi/DeMaskedCore.json';
+
+
+export const CONTRACT_ADDRESSES = {
+    DeMaskedToken: '0x747bf0477e71F8e308D778A41d24833F6bece64e',
+    DeMaskedCore: '0x0f45c66B4Bc1eed5E2c35B119c4987dc96eC14bD',
+}
+
+export const CONTRACT_ABIS = {
+    DeMaskedToken: DeMaskedTokenABI,
+    DeMaskedCore: DeMaskedCoreABI,
+}
+
+export const FEES_DECIMALS = 18;
+
